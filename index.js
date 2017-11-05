5bdcc9778d69af3207a18641889e575bcad88a53
Scott Cooper <scttcper@gmail.com>
2017-11-05 11:12:37 -0800

    fix avatars, use hapi

--- index.js
+++ index.js
@@ -1,64 +1,62 @@
-const Koa = require('koa');
-const cors = require('kcors');
-const got = require('got');
+const request = require('request-promise-native');
 const isNumber = require('is-number');
-const redis = require('redis');
+const Hapi = require('hapi');
+const Joi = require('joi');
 
-const app = new Koa();
-module.exports = app;
-app.use(cors());
-
-const client = redis.createClient({
-  url: process.env.REDIS_URL || null,
-});
+const server = Hapi.server({ port: 5000 });
+module.exports = server;
 
 const opt = {
   method: 'HEAD',
   timeout: 900,
-  retries: 2,
-  encoding: null,
+  uri: 'https://www.heroesofnewerth.com/getAvatar_SSL.php',
+  qs: {
+    id: ''
+  },
+  followRedirect: false,
+  resolveWithFullResponse: true,
+  simple: false,
 };
-const DEFAULT_AVATAR = 'https://s3.amazonaws.com/naeu-icb2/icons/default/account/default.png';
+const DEFAULT_AVATAR =
+  'https://s3.amazonaws.com/naeu-icb2/icons/default/account/default.png';
 
-function getAvatar(accountId) {
-  const url = `https://www.heroesofnewerth.com/getAvatar_SSL.php?id=${accountId}`;
-  return got(url, opt)
-    .then((res) => {
-      return res.url;
-    })
-    .catch(() => {
-      return DEFAULT_AVATAR;
-    });
-}
-
-function findOrGet(accountId) {
-  return new Promise((resolve) => {
-    client.get(accountId, (err, reply) => {
-      resolve(reply);
-    });
-  }).then((link) => {
-    if (!link) {
-      return getAvatar(accountId);
+async function getAvatar(accountId) {
+  opt.qs.id = accountId;
+  try {
+    const res = await request(opt);
+    if (res.headers.location.includes('icons//')) {
+      return res.headers.location.replace('icons//', 'icons/');
     }
-    return link;
-  });
+    return DEFAULT_AVATAR;
+  } catch (e) {
+    return DEFAULT_AVATAR;
+  }
 }
 
-app.use(async (ctx, next) => {
-  ctx.assert(ctx.req.url.length < 11, 400, 'invalid length');
-  const str = ctx.req.url.split('/')[1];
-  if (!isNumber(str)) {
-    ctx.body = DEFAULT_AVATAR;
-    return next();
-  }
-  const accountId = parseInt(ctx.req.url.split('/')[1], 10);
-  ctx.body = await findOrGet(accountId);
-  client.set(accountId, ctx.body, 'PX', 86400000, 'NX');
-  return next();
+server.route({
+  method: 'GET',
+  path: '/{id?}',
+  config: {
+    validate: {
+      params: {
+        id: Joi.string().min(0).max(11),
+      },
+    },
+    cors: true,
+    cache: {
+      expiresIn: 7200, // 120 min
+    },
+    handler: async function(request, h) {
+      if (!isNumber(request.params.id)) {
+        return DEFAULT_AVATAR;
+      }
+      return await getAvatar(request.params.id);
+    },
+  },
 });
 
 /* istanbul ignore if */
 if (!module.parent) {
-  app.listen(5000);
-  console.log('Listening on 5000');
+  server.start();
+  console.log('Listening on http://localhost:5000');
 }
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,12 +1,12 @@
 const request = require('supertest');
 const assert = require('assert');
 
-const app = require('../index');
+const server = require('../index');
 
 describe('avatar', function () {
   this.timeout(10000);
   it('should return avatar', function () {
-    return request(app.listen())
+    return request(server.listener)
       .get('/7619944')
       .expect(200)
       .then((res) => {
@@ -14,27 +14,22 @@ describe('avatar', function () {
       });
   });
   it('should return default', function () {
-    return request(app.listen())
+    return request(server.listener)
       .get('/12345')
       .expect(200, 'https://s3.amazonaws.com/naeu-icb2/icons/default/account/default.png');
   });
   it('should catch default', function () {
-    return request(app.listen())
+    return request(server.listener)
       .get('/2')
       .expect(200, 'https://s3.amazonaws.com/naeu-icb2/icons/default/account/default.png');
   });
   it('should check length', function () {
-    return request(app.listen())
-      .get('/1234544444/345345')
-      .expect(400);
-  });
-  it('should check length', function () {
-    return request(app.listen())
-      .get('/1234544444/345345')
+    return request(server.listener)
+      .get('/12345444441123')
       .expect(400);
   });
   it('should check is number', function () {
-    return request(app.listen())
+    return request(server.listener)
       .get('/hello')
       .expect(200, 'https://s3.amazonaws.com/naeu-icb2/icons/default/account/default.png');
   });
